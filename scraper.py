c4f9bb5cea6c8f0eecfe4329a384fcedb2c7e6f1
Marcello Seri <marcello.seri@gmail.com>
2015-02-05 20:37:11 +0000

    Try to solve No 'Access-Control-Allow-Origin' error

--- scraper.py
+++ scraper.py
@@ -10,6 +10,19 @@ def root():
   return ''
 
 @app.route('/json/london_analysis_seminar')
-
 def LAS():
   return jsonifyLAS().cache
+
+# try to solve No 'Access-Control-Allow-Origin' error
+# http://mortoray.com/2014/04/09/allowing-unlimited-access-with-cors/
+@app.after_request
+def add_cors(resp):
+    """ Ensure all responses have the CORS headers. This ensures any failures are also accessible
+        by the client. """
+    resp.headers['Access-Control-Allow-Origin'] = flask.request.headers.get('Origin','*')
+    resp.headers['Access-Control-Allow-Credentials'] = 'true'
+    resp.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS, GET'
+
+    if app.debug:
+        resp.headers['Access-Control-Max-Age'] = '1'
+    return resp
